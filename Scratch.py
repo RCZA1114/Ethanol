29faa597dc14f3bf36767beccd44bfe56d41905c
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-12-27 06:46:11 +0800

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,19 +1,27 @@
+import mysql.connector
+import pandas as pd
+#from streamlit_autorefresh import st_autorefresh
 
+def get_data():
+    mydb = mysql.connector.connect(    #GEt The Data from the Database
+        host="127.0.0.1",
 
+        user="ramon",
+        passwd="pythos",
+        database="trial",
+        port=3307,
+    )
+    
+    sql_query = pd.read_sql(
+       'SELECT * FROM trial.`data`',
+        mydb,
+    )
 
-"""
-measure = st.selectbox("Select Measurement", ('Mean', 'Standard Deviation'))
-if measure == "Mean":
-    wlm =df['WL(nm)'].mean()
-    absm = df['Abs'].mean()
-    st.write(f"The Mean of WL(nm) is {wlm} and Abs is {absm}. ")
-elif measure == "Standard Deviation":
-    wlm =df['WL(nm)'].std()
-    absm = df['Abs'].std()
-    st.write(f"The Standard Deviation of of WL(nm) is {wlm} and Abs is {absm}. ")
+    data = pd.DataFrame(sql_query)
 
-UCLWL = df['WL(nm)'].mean() + (3*df['WL(nm)'].std())
-LCLWL = df['WL(nm)'].mean() - (3*df['WL(nm)'].std())
-UCLABS = df['Abs'].mean() + (3*df['Abs'].std())
-LCLABS = df['Abs'].mean() - (3*df['Abs'].std())
-"""
+    
+    return data
+data = get_data()
+
+print(data.head(5))
+
